752a7825db8a81ea5fe26c9f13c3eb1ef39db1d6
pooja <pk537@njit.edu>
2018-10-13 23:35:31 -0400

    Fix: Bootstrap

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,9 +1,10 @@
 meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 <meta name="description" content="Learana Learning Analytics">
 <meta name="author" content="keith">
 
-<title>Learana - Learning Analytics</title>
+<title>IS601-LARAVEL</title>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
       integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
